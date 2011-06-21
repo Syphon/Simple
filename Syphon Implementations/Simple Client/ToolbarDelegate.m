4032afcbdaf2bda4c4f9aba7ea8ec2361281d3cc
Tom Butterworth <bangnoise@gmail.com>
2011-06-21 23:11:20 +0000

    Fix leak in toolbar, add license text to new files in Simple Client.

--- Syphon Implementations/Simple Client/ToolbarDelegate.m
+++ Syphon Implementations/Simple Client/ToolbarDelegate.m
@@ -1,10 +1,31 @@
-//
-//  ToolbarDelegate.m
-//  Simple Client
-//
-//  Created by Tom on 20/06/2011.
-//  Copyright 2011 Tom Butterworth. All rights reserved.
-//
+/*
+ ToolbarDelegate.m
+ Syphon (SDK)
+ 
+ Copyright 2011 bangnoise (Tom Butterworth) & vade (Anton Marini).
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "ToolbarDelegate.h"
 
@@ -48,7 +69,7 @@
     else if ([itemIdentifier isEqualToString:@"FixedWidthItemIdentifier"])
     {
         // This keeps the status centered unless the window is small, at which point it shrinks out of the way
-        NSView *empty = [[NSView alloc] initWithFrame:[availableServersMenu frame]];
+        NSView *empty = [[[NSView alloc] initWithFrame:[availableServersMenu frame]] autorelease];
         [item setView:empty];
         [item setMinSize:(NSSize){0.0, [empty frame].size.height}];
         [item setMaxSize:[empty frame].size];
